a0ae0316ea44af0696929c4d07cdc0b36dfd97b2
Alex <alexunc@mail.ru>
2015-12-26 16:11:46 +0500

    New TreeMap 1.

--- src/com/autocomplete/ScanText.java
+++ src/com/autocomplete/ScanText.java
@@ -9,7 +9,7 @@ import java.util.*;
 public class ScanText {
     TreeMap<String, String> dictionaryMap;
     TreeMap<String, String> tempTreeMap;
-    LinkedList<String> spisokSlov;
+    ArrayList<String> spisokSlov;
 
 
 
@@ -18,7 +18,7 @@ public class ScanText {
 
     public void parseFile() throws IOException{
         dictionaryMap = new TreeMap();
-        spisokSlov = new LinkedList<>();
+        spisokSlov = new ArrayList<>(1000000);
         final BufferedReader br = new BufferedReader(new InputStreamReader(
                 new FileInputStream("resources\\test.in"), "Cp1251"));
         String str;
@@ -40,32 +40,68 @@ public class ScanText {
 
     public void findMatches() {
         //tempTreeMap = new TreeMap();
-
+        long before = System.currentTimeMillis();
+        StringBuilder stringBuilder = new StringBuilder();
         for (int i=0;i< spisokSlov.size();i++) {
             int count = 0;
 
             for (final Map.Entry e : dictionaryMap.entrySet()) {
-
-
-                if (e.getKey().toString().startsWith(spisokSlov.get(i))){
-
-                    //System.out.println(e);
-                    //tempTreeMap.put(e.getKey(), e.getValue());
-                    System.out.println(spisokSlov.get(i) + " " + e.getKey() + " " + e.getValue());
-                    count++;
-                    if (e.getKey()==null) break;
-
-                }
-                if (count >= 10) break;
-
+//
+//
+//                if (e.getKey().toString().startsWith(spisokSlov.get(i))){
+//
+//                    //System.out.println(e);
+//                    //tempTreeMap.put(e.getKey(), e.getValue());
+//                    //System.out.println(spisokSlov.get(i) + " " + e.getKey() + " " + e.getValue());
+//                    //stringBuilder.append(e.getKey()).append("\n").toString();
+//                    //System.out.println(stringBuilder);
+//                    count++;
+//                    if (e.getKey()==null) break;
+//
+//                }
+//                if (count >= 10) break;
+//
             }
+
                 //System.out.println("--------------------------------------");
             }
+        long after = System.currentTimeMillis();
+        System.out.println((after - before)/1000 + "s.");
 //            List linkedList = new ArrayList(Sorter.SortByValue(tempTreeMap));
 //            for (int f = (linkedList.size() - 1); f > (linkedList.size() - 11); f--) {
 //                System.out.println(linkedList.get(f));
 //            }
         }
 
-    }
+
+//    public void findMatchesWithSubMap() {
+//        //tempTreeMap = new TreeMap();
+//
+//        for (int i=0;i< spisokSlov.size();i++) {
+//            int count = 0;
+//
+//            for (final Map.Entry e : dictionaryMap.entrySet()) {
+//
+//
+//                if (e.getKey().toString().startsWith(spisokSlov.get(i))){
+//
+//                    //System.out.println(e);
+//                    //tempTreeMap.put(e.getKey(), e.getValue());
+//                    System.out.println(spisokSlov.get(i) + " " + e.getKey() + " " + e.getValue());
+//                    count++;
+//                    if (e.getKey()==null) break;
+//
+//                }
+//                if (count >= 10) break;
+//
+//            }
+//            //System.out.println("--------------------------------------");
+//        }
+////            List linkedList = new ArrayList(Sorter.SortByValue(tempTreeMap));
+////            for (int f = (linkedList.size() - 1); f > (linkedList.size() - 11); f--) {
+////                System.out.println(linkedList.get(f));
+////            }
+//    }
+
+}
 
