56f17823230f60cb0bf1ec04534f7b2ce2163640
Alex <alexunc@mail.ru>
2015-12-28 22:59:08 +0500

    Make some order in ValuesComparator.class.

--- src/com/autocomplete/ValuesComparator.java
+++ src/com/autocomplete/ValuesComparator.java
@@ -7,9 +7,17 @@ import java.util.Map;
 /**
  * Класс, который сортирует Map по значениям.
  */
-public class ValuesComparator implements Comparator{
+public class ValuesComparator implements Comparator {//NOPMD
+
+    /**
+     * Compare values.
+     *
+     * @param o1 first value.
+     * @param o2 second value.
+     * @return result of the comparison.
+     */
     @Override
-    public int compare(final Object o1, final Object o2) {
+    public final int compare(final Object o1, final Object o2) {
         final Map.Entry e1 = (Map.Entry) o1;
         final Map.Entry e2 = (Map.Entry) o2;
         final Comparable c1 = (Comparable) e1.getValue();
