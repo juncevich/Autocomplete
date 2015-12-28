ff8259e9b1447051048b84f75e7178c6a2313bb8
Alex <alexunc@mail.ru>
2015-12-28 22:44:28 +0500

    Make some order.

--- src/com/autocomplete/ScanText.java
+++ src/com/autocomplete/ScanText.java
@@ -6,45 +6,67 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
-public class ScanText {
-    TreeMap dictionaryMap;
-    ArrayList<String> listWords;
+import static java.lang.System.currentTimeMillis;
+import static java.lang.System.out;
+
+/**
+ * Class, that scan file with text and parse it.
+ */
+public class ScanText {//NOPMD
+    /**
+     * List, with sorted words and numbers of repetitions.
+     */
+    private Map<String, String> dictionaryMap;
+    /**
+     * List, with test words.
+     */
+    private List<String> listWords;
+    /**
+     * Collection, that's used to sort output words
+     * by amount of using.
+     */
+    private Map<String, String> tempTreeMap;
 
 
-
-
-
-
-    public void parseFile() throws IOException{
-        dictionaryMap = new TreeMap();
-        listWords = new ArrayList<>(1000000);
-        final BufferedReader br = new BufferedReader(new InputStreamReader(
+    /**
+     * Method, that parse text file and put info to appropriate collections.
+     *
+     * @throws IOException This is exceptions.
+     */
+    public final void parseFile() throws IOException {
+        dictionaryMap = new TreeMap<>();
+        listWords = new ArrayList<>();
+        final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(
                 new FileInputStream("resources\\test.in"), "Cp1251"));
         String str;
 
 
-        final int kolvoZapisSlovar = Integer.valueOf(br.readLine());
-        for (int i = 0; i < kolvoZapisSlovar; i++) {
-            str = br.readLine();
+        final int numberOfEntriesInTheDictionary = Integer.valueOf(bufferedReader.readLine());
+        for (int i = 0; i < numberOfEntriesInTheDictionary; i++) {
+            str = bufferedReader.readLine();
             final String[] fields = str.split(" ");
             dictionaryMap.put(fields[0], fields[1]);
         }
-        final int kolvoSlov = Integer.valueOf(br.readLine());
-        for (int i = 0; i < kolvoSlov; i++) {
-            str = br.readLine();
+        final int numberOfWords = Integer.valueOf(bufferedReader.readLine());
+        for (int i = 0; i < numberOfWords; i++) {
+            str = bufferedReader.readLine();
             listWords.add(str);
         }
-        System.out.println(dictionaryMap.lastKey());
+
     }
 
-    public void findMatches() {
+    /**
+     * This method working with collections of information.
+     * Correlates words with
+     */
+    public final void findMatches() {
 
-        long before = System.currentTimeMillis();
-        StringBuilder stringBuilder = new StringBuilder();
-        for (String word : listWords) {
+        final long before = currentTimeMillis();
+        for (final String word : listWords) {
             int count = 0;
 
             for (final Map.Entry<String, String> e : dictionaryMap.entrySet()) {
@@ -52,59 +74,32 @@ public class ScanText {
 
                 if (e.getKey().startsWith(word)) {
 
-                    //System.out.println(e);
-                    //tempTreeMap.put(e.getKey(), e.getValue());
-                    //System.out.println(listWords.get(i) + " " + e.getKey() + " " + e.getValue());
-                    //stringBuilder.append(e.getKey()).append("\n").toString();
-                    //System.out.println(stringBuilder);
+                    out.println(e);
+                    tempTreeMap.put(e.getKey(), e.getValue());
+                    out.println(e.getKey() + " " + e.getValue());
                     count++;
-                    if (e.getKey() == null) break;
-
-
+                    if (e.getKey() == null) {
+                        break;
+                    }
                 }
-                if (count >= 10) break;
+                if (count >= 10) {
+                    break;
+                }
 
             }
 
-            System.out.println("--------------------------------------");
+            out.println("--------------------------------------");
         }
-        long after = System.currentTimeMillis();
-        System.out.println((after - before)/1000 + "s.");
-//            List linkedList = new ArrayList(Sorter.SortByValue(tempTreeMap));
-//            for (int f = (linkedList.size() - 1); f > (linkedList.size() - 11); f--) {
-//                System.out.println(linkedList.get(f));
-//            }
+        final long after = currentTimeMillis();
+        out.println((after - before) / 1000 + "s.");
+        @SuppressWarnings("unchecked")
+        final List linkedList =
+                new ArrayList<>(Sorter.SortByValue((TreeMap) tempTreeMap));
+        for (int f = (linkedList.size() - 1);
+             f > (linkedList.size() - 11); f--) {
+            out.println(linkedList.get(f));
         }
-
-
-//    public void findMatchesWithSubMap() {
-//        //tempTreeMap = new TreeMap();
-//
-//        for (int i=0;i< listWords.size();i++) {
-//            int count = 0;
-//
-//            for (final Map.Entry e : dictionaryMap.entrySet()) {
-//
-//
-//                if (e.getKey().toString().startsWith(listWords.get(i))){
-//
-//                    //System.out.println(e);
-//                    //tempTreeMap.put(e.getKey(), e.getValue());
-//                    System.out.println(listWords.get(i) + " " + e.getKey() + " " + e.getValue());
-//                    count++;
-//                    if (e.getKey()==null) break;
-//
-//                }
-//                if (count >= 10) break;
-//
-//            }
-//            //System.out.println("--------------------------------------");
-//        }
-////            List linkedList = new ArrayList(Sorter.SortByValue(tempTreeMap));
-////            for (int f = (linkedList.size() - 1); f > (linkedList.size() - 11); f--) {
-////                System.out.println(linkedList.get(f));
-////            }
-//    }
+        }
 
 }
 
