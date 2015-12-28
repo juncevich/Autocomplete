7985d99ce666be11505a1e8de208a0d7ff69f265
Alex <alexunc@mail.ru>
2015-12-28 21:27:44 +0500

    Some refactoring.

--- src/com/autocomplete/ScanText.java
+++ src/com/autocomplete/ScanText.java
@@ -1,15 +1,17 @@
 package com.autocomplete;
 
 
-import sun.reflect.generics.tree.Tree;
-
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class ScanText {
-    TreeMap<String, String> dictionaryMap;
-    TreeMap<String, String> tempTreeMap;
-    ArrayList<String> spisokSlov;
+    TreeMap dictionaryMap;
+    ArrayList<String> listWords;
 
 
 
@@ -18,7 +20,7 @@ public class ScanText {
 
     public void parseFile() throws IOException{
         dictionaryMap = new TreeMap();
-        spisokSlov = new ArrayList<>(1000000);
+        listWords = new ArrayList<>(1000000);
         final BufferedReader br = new BufferedReader(new InputStreamReader(
                 new FileInputStream("resources\\test.in"), "Cp1251"));
         String str;
@@ -33,38 +35,39 @@ public class ScanText {
         final int kolvoSlov = Integer.valueOf(br.readLine());
         for (int i = 0; i < kolvoSlov; i++) {
             str = br.readLine();
-            spisokSlov.add(str);
+            listWords.add(str);
         }
         System.out.println(dictionaryMap.lastKey());
     }
 
     public void findMatches() {
-        //tempTreeMap = new TreeMap();
+
         long before = System.currentTimeMillis();
         StringBuilder stringBuilder = new StringBuilder();
-        for (int i=0;i< spisokSlov.size();i++) {
+        for (String word : listWords) {
             int count = 0;
 
-            for (final Map.Entry e : dictionaryMap.entrySet()) {
-//
-//
-//                if (e.getKey().toString().startsWith(spisokSlov.get(i))){
-//
-//                    //System.out.println(e);
-//                    //tempTreeMap.put(e.getKey(), e.getValue());
-//                    //System.out.println(spisokSlov.get(i) + " " + e.getKey() + " " + e.getValue());
-//                    //stringBuilder.append(e.getKey()).append("\n").toString();
-//                    //System.out.println(stringBuilder);
-//                    count++;
-//                    if (e.getKey()==null) break;111
-//
-//                }
-//                if (count >= 10) break;
-//
+            for (final Map.Entry<String, String> e : dictionaryMap.entrySet()) {
+
+
+                if (e.getKey().startsWith(word)) {
+
+                    //System.out.println(e);
+                    //tempTreeMap.put(e.getKey(), e.getValue());
+                    //System.out.println(listWords.get(i) + " " + e.getKey() + " " + e.getValue());
+                    //stringBuilder.append(e.getKey()).append("\n").toString();
+                    //System.out.println(stringBuilder);
+                    count++;
+                    if (e.getKey() == null) break;
+
+
+                }
+                if (count >= 10) break;
+
             }
 
-                //System.out.println("--------------------------------------");
-            }
+            System.out.println("--------------------------------------");
+        }
         long after = System.currentTimeMillis();
         System.out.println((after - before)/1000 + "s.");
 //            List linkedList = new ArrayList(Sorter.SortByValue(tempTreeMap));
@@ -77,17 +80,17 @@ public class ScanText {
 //    public void findMatchesWithSubMap() {
 //        //tempTreeMap = new TreeMap();
 //
-//        for (int i=0;i< spisokSlov.size();i++) {
+//        for (int i=0;i< listWords.size();i++) {
 //            int count = 0;
 //
 //            for (final Map.Entry e : dictionaryMap.entrySet()) {
 //
 //
-//                if (e.getKey().toString().startsWith(spisokSlov.get(i))){
+//                if (e.getKey().toString().startsWith(listWords.get(i))){
 //
 //                    //System.out.println(e);
 //                    //tempTreeMap.put(e.getKey(), e.getValue());
-//                    System.out.println(spisokSlov.get(i) + " " + e.getKey() + " " + e.getValue());
+//                    System.out.println(listWords.get(i) + " " + e.getKey() + " " + e.getValue());
 //                    count++;
 //                    if (e.getKey()==null) break;
 //
